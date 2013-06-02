738cc1cb8c88d42a2dd173e5e15984696d97ed7c
Toby Sims <toby.sims@gmail.com>
2013-06-02 09:38:50 +0100

    Initial commit with foundation installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arthouse::Application.config.secret_token = '16309b8800ddd15f92c579e2f99303580a208d5221092a51c10f09611fbf8b2af54ba04fb406f0d1b664f188dbc198098ce430a9a5786bb22197a435d1b27e3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Arthouse::Application.config.session_store :cookie_store, key: '_Arthouse_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Arthouse::Application.config.session_store :active_record_store
