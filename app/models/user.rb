f8863387bca48fb8e6b8e29c535522281056eee6
Toby Sims <toby.sims@gmail.com>
2013-06-03 18:31:27 +0100

    half way through password and login implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 require 'digest/md5'
-require 
+require 'bcrypt'
 
 class User < ActiveRecord::Base
   has_many :galleries
   validates :name_first, :name_last, :email, presence: true
+  validates_presence_of :password, :on => :create
   attr_accessible :email, :name_first, :name_last
+  attr_accessor :password, :password_confirmation
+
+  #has_secure password
 
   before_validation :downcase_email
 
@@ -16,7 +20,23 @@ class User < ActiveRecord::Base
     Digest::MD5.hexdigest(self.email)  
   end
 
+  def authenticate(password)
+    if self.hashed == BCrypt::Engine.hash_secret(password, self.salt)
+      self
+    else
+      nil
+    end
+  end
+
   protected
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.hashed = BCrypt::Engine.hash_secret(password, self.salt)
+      self.password = nil
+    end
+  end
 
   def downcase_email
     self.email.downcase!
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130603135814) do
+ActiveRecord::Schema.define(:version => 20130603173017) do
 
   create_table "galleries", :force => true do |t|
     t.string   "title"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(:version => 20130603135814) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "password"
+    t.string   "hashed"
   end
 
 end
